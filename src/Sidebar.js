8dfdd0ea1d27e66b82801ad2f20da48a961b0253
Alex Peter <apyells@gmail.com>
2019-06-10 11:41:30 -0500

    init commit

---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <p>Sidebar</p> 
+        )
+    }
+}
+
+export default Sidebar
